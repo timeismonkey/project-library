9a3d486a2ceb5dab2367d2e2e50ccb06be3d376d
jerryDesk <jerrykeiko98@gmail.com>
2024-02-04 13:20:03 -0800

    Fomratted displayBooks().

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    // this.info = () => read ? `${this.title} by ${this.author}, ${this.pages} pages, read` : `${this.title} by ${this.author}, ${this.pages} pages, not read yet`
 }
 
 Book.prototype.info = function () {
@@ -17,17 +16,19 @@ Book.prototype.info = function () {
 
 function addBookToLibrary() {
     // do stuff here
-    // Take users form input and create a book object from it
-    // Add the book to library
 }
 
-// Display books in myLibrary on page
+// Display books in table on page
 function displayBooks() {
-    myLibrary.forEach(book => {
-        // Add book as a row in .library
-        libraryBody.innerHTML += `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.read ? 'Yes' : 'No'}</td></tr>`;
-    })
-} 
+    myLibrary.forEach(
+        (book) =>
+            (libraryBody.innerHTML += `<tr><td>${book.title}</td><td>${
+                book.author
+            }</td><td>${book.pages}</td><td>${
+                book.read ? 'Yes' : 'No'
+            }</td></tr>`)
+    );
+}
 
 // Test
 
@@ -37,9 +38,9 @@ const book2 = new Book('Book2', 'you', 35, true);
 const book3 = new Book('Book3', 'we', 40, false);
 const book4 = new Book('Book4', 'he', 45, false);
 
-myLibrary.push(book1)
-myLibrary.push(book2)
-myLibrary.push(book3)
-myLibrary.push(book4)
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
 
 displayBooks();
