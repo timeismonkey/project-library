92aeb69270b07178bc08d19292d1cc6cb929a62e
jerryDesk <jerrykeiko98@gmail.com>
2024-02-05 17:15:54 -0800

    Added addBookToLibrary() functionality.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const myLibrary = [];
 const libraryBody = document.querySelector('.library-body');
+const addBookBtn = document.querySelector('#add-book');
 const bookDialog = document.querySelector('#book-dialog');
+const bookForm = document.querySelector('#book-form');
+const cancelModal = document.querySelector('#cancel-button');
+const confirmModal = document.querySelector('#confirm-button');
+
+const myLibrary = [];
+console.log(myLibrary);
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -16,11 +22,15 @@ Book.prototype.info = function () {
 };
 
 function addBookToLibrary() {
-    // do stuff here
-}
-
-function showDialog () {
-    
+    // Deal with form input
+    const inputs = bookForm.querySelectorAll('input');
+    const title = inputs[0].value;
+    const author = inputs[1].value;
+    const pages = inputs[2].value;
+    const read = bookForm.querySelector('#read').value === 'No' ? false : true;
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+    displayBooks();
 }
 
 // Display books in table on page
@@ -30,25 +40,34 @@ function displayBooks() {
             (libraryBody.innerHTML += `<tr><td>${book.title}</td><td>${
                 book.author
             }</td><td>${book.pages}</td><td>${
-                book.read ? 'Yes' : 'No'
+                book.read ? 'yes' : 'no'
             }</td></tr>`)
     );
 }
 
-
+addBookBtn.addEventListener('click', () => bookDialog.showModal());
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+});
+cancelModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    bookDialog.close();
+});
+confirmModal.addEventListener('click', (e) => bookDialog.close())
 
 
 // Test
 
 // Add test books
-const book1 = new Book('Book1', 'me', 30, false);
-const book2 = new Book('Book2', 'you', 35, true);
-const book3 = new Book('Book3', 'we', 40, false);
-const book4 = new Book('Book4', 'he', 45, false);
+// const book1 = new Book('Book1', 'me', 30, false);
+// const book2 = new Book('Book2', 'you', 35, true);
+// const book3 = new Book('Book3', 'we', 40, false);
+// const book4 = new Book('Book4', 'he', 45, false);
 
-myLibrary.push(book1);
-myLibrary.push(book2);
-myLibrary.push(book3);
-myLibrary.push(book4);
+// myLibrary.push(book1);
+// myLibrary.push(book2);
+// myLibrary.push(book3);
+// myLibrary.push(book4);
 
 displayBooks();
